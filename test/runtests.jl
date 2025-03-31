3665f0d822dd0de684fb3806c60aa705267cee18
abhro <5664668+abhro@users.noreply.github.com>
2025-03-31 13:18:18 -0400

    Encapsulate all tests into one testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,9 @@ using Unitful, UnitfulAstro, Measurements
 
 Random.seed!(8675309)
 
-include("spectrum.jl")
-include("utils.jl")
-include("transforms/transforms.jl")
-include("plotting.jl")
+@testset "Spectra.jl" begin
+    include("spectrum.jl")
+    include("utils.jl")
+    include("transforms/transforms.jl")
+    include("plotting.jl")
+end
