a1dc86dfe86eb4356b97319cea503a4245e7e7cd
icweaver <weaveric@gmail.com>
2025-04-30 18:46:34 -0700

    added echelle spec plot tests

--- test/plotting.jl
+++ test/plotting.jl
@@ -1,7 +1,7 @@
 using RecipesBase, Unitful
 import Measurements: value
 
-@testset "Plotting" begin
+@testset "Plotting - Single" begin
     wave = range(1e4, 3e4, length = 1000)
     sigma = 0.1 .* sin.(wave)
     T = 6700
@@ -11,8 +11,8 @@ import Measurements: value
     spec = spectrum(wave, flux, name = "Test Spectrum")
 
     rec = RecipesBase.apply_recipe(Dict{Symbol,Any}(), spec)
-    @test getfield(rec[1], 1) == Dict{Symbol,Any}(:label => "", 
-        :xlabel => "wave", 
+    @test getfield(rec[1], 1) == Dict{Symbol,Any}(:label => "",
+        :xlabel => "wave",
         :ylabel => "flux density",
         :seriestype => :step)
     @test rec[1].args == (ustrip.(spec.wave), value.(ustrip.(spec.flux)))
@@ -21,9 +21,24 @@ import Measurements: value
     strip_spec = ustrip(spec)
 
     rec = RecipesBase.apply_recipe(Dict{Symbol,Any}(), strip_spec)
-    @test getfield(rec[1], 1) == Dict{Symbol,Any}(:label => "", 
-        :xlabel => "wave", 
+    @test getfield(rec[1], 1) == Dict{Symbol,Any}(:label => "",
+        :xlabel => "wave",
         :ylabel => "flux density",
         :seriestype => :step)
     @test rec[1].args == (strip_spec.wave, value.(strip_spec.flux))
+end
+
+@testset "Plotting - Echelle" begin
+    n_orders = 10
+    wave = reshape(range(100, 1e4, length=1000), 100, n_orders)'
+    flux = ones(n_orders, 100) .* collect(1:n_orders)
+    spec = spectrum(wave, flux, name = "Test Echelle Spectrum")
+    rec  = RecipesBase.apply_recipe(Dict{Symbol,Any}(), spec)
+    @test getfield(rec[1], 1) == Dict{Symbol,Any}(
+          :label => ["Order $(i)" for i in 1:n_orders],
+          :xlabel => "wave",
+          :ylabel => "flux density",
+          :seriestype => :step,
+    )
+    @test rec[1].args == (spec.wave', spec.flux')
 end
