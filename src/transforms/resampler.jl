8a2dbc0185e36093432080d1bc26e5bb77051747
cgarling <chris.t.garling@gmail.com>
2025-11-03 08:38:55 -0500

    Expand `SpectrumResampler` docstring

--- src/transforms/resampler.jl
+++ src/transforms/resampler.jl
@@ -3,18 +3,55 @@
 
 Type representing the spectrum `spec` with interpolator `interp`.
 
-```jldoctest
+Interpolation methods from many packages can be used without issue. Below we show example usage of [DataInterpolations.jl](https://github.com/SciML/DataInterpolations.jl) and [Interpolations.jl](https://github.com/JuliaMath/Interpolations.jl).
+
+First, we set up a an arbitrary spectrum and a linear interpolator from DataInterpolations.jl
+```jldoctest resampling
 julia> using Spectra: spectrum, flux, wave, SpectrumResampler
 
 julia> using DataInterpolations: LinearInterpolation, ExtrapolationType
 
 julia> spec = spectrum([20, 40, 120, 160, 200], [1, 3, 7, 6, 20]);
 
-julia> interp = LinearInterpolation(flux(spec), wave(spec); extrapolation = ExtrapolationType.Constant);
+julia> interp = LinearInterpolation(flux(spec), wave(spec); 
+                    extrapolation = ExtrapolationType.Constant);
+```
 
+Now, we construct the `SpectrumResampler` and define the new wavelength grid that we want to resample the original spectrum to.
+
+```jldoctest resampling
 julia> resampler = SpectrumResampler(spec, interp);
 
 julia> wave_sampled = [10, 50, 90, 130, 140, 170, 210, 220, 230];
+```
+
+To perform the resampling, you call the resampler with the desired wavelength grid.
+
+```jldoctest resampling
+julia> result = resampler(wave_sampled);
+```
+
+The resampled wavelength and flux can be obtained with the `wave` and `flux` methods.
+
+```jldoctest resampling
+julia> result isa SpectrumResampler
+true
+
+julia> wave(result) == wave_sampled
+true
+
+julia> flux(result) == interp(wave_sampled)
+true
+```
+
+Use of [Interpolations.jl](https://github.com/JuliaMath/Interpolations.jl) follows the same general procedure but using a different `interp`.
+
+```jldoctest resampling
+julia> using Interpolations: linear_interpolation, Flat
+
+julia> interp = linear_interpolation(wave(spec), flux(spec); extrapolation_bc = Flat());
+
+julia> resampler = SpectrumResampler(spec, interp);
 
 julia> result = resampler(wave_sampled);
 
@@ -24,7 +61,7 @@ true
 julia> wave(result) == wave_sampled
 true
 
-julia> flux(result) == interp.(wave_sampled)
+julia> flux(result) == interp(wave_sampled)
 true
 ```
 """
