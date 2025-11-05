e2ceff8dc50f785ada7799e4d58a1b61f190d53d
icweaver <weaveric@gmail.com>
2025-11-04 18:24:42 -0800

    drop mutation tests, using immutable structs in #35

--- test/transforms/resample.jl
+++ test/transforms/resample.jl
@@ -17,17 +17,17 @@ function resample(spec1::AbstractSpectrum, spec2::AbstractSpectrum)
     return resample(spec1, new_wave)
 end
 
-function resample!(spec::AbstractSpectrum, new_wave)
-    spec_new = resample(spec, new_wave)
-    spec.flux = Spectra.flux(spec_new)
-    spec.wave = Spectra.wave(spec_new)
-    return spec
-end
+#function resample!(spec::AbstractSpectrum, new_wave)
+#    spec_new = resample(spec, new_wave)
+#    spec.flux = Spectra.flux(spec_new)
+#    spec.wave = Spectra.wave(spec_new)
+#    return spec
+#end
 
-function resample!(spec1::AbstractSpectrum, spec2::AbstractSpectrum)
-    new_wave = Spectra.wave(spec2)
-    resample!(spec1, new_wave)
-end
+#function resample!(spec1::AbstractSpectrum, spec2::AbstractSpectrum)
+#    new_wave = Spectra.wave(spec2)
+#    resample!(spec1, new_wave)
+#end
 
 function mock_spectrum(n::Int = Int(1e3); use_units::Bool = false)
     wave = range(1e4, 3e4, length = n)
@@ -56,9 +56,9 @@ end
         @test Spectra.wave(res_spec) == new_wave
         @test length(Spectra.flux(res_spec)) == length(new_wave)
 
-        resample!(spec, new_wave)
-        @test Spectra.wave(res_spec) == spec.wave
-        @test Spectra.flux(res_spec) == spec.flux
+        #resample!(spec, new_wave)
+        #@test Spectra.wave(res_spec) == spec.wave
+        #@test Spectra.flux(res_spec) == spec.flux
 
         # Unitful
         spec = mock_spectrum(use_units=true)
@@ -69,9 +69,9 @@ end
         @test Spectra.wave(res_spec) == new_wave
         @test length(Spectra.flux(res_spec)) == length(new_wave)
 
-        resample!(spec, new_wave)
-        @test Spectra.wave(res_spec) == spec.wave
-        @test Spectra.flux(res_spec) == spec.flux
+        #resample!(spec, new_wave)
+        #@test Spectra.wave(res_spec) == spec.wave
+        #@test Spectra.flux(res_spec) == spec.flux
 
         # Test resampling to another Spectrum
         spec1 = mock_spectrum(Integer(1e4))
@@ -79,9 +79,9 @@ end
         res_spec = resample(spec1, spec2)
         @test Spectra.wave(res_spec) == Spectra.wave(spec2)
 
-        resample!(spec1, spec2)
-        @test Spectra.wave(spec1) == spec2.wave
-        @test Spectra.flux(spec1) == Spectra.flux(res_spec)
+        #resample!(spec1, spec2)
+        #@test Spectra.wave(spec1) == spec2.wave
+        #@test Spectra.flux(spec1) == Spectra.flux(res_spec)
 
         # Unitful
         spec1 = mock_spectrum(Integer(1e4), use_units=true)
@@ -92,17 +92,17 @@ end
         @test unit(eltype(Spectra.flux(spec1))) == unit(eltype(Spectra.flux(spec2))) == unit(eltype(Spectra.flux(res_spec)))
 
         # Test when spectra2 has different units
-        spec1 = mock_spectrum(Integer(1e4), use_units=true)
-        spec2 = mock_spectrum(Integer(1e3), use_units=true)
-        spec1.wave = uconvert.(u"cm", spec1.wave)
-        resample!(spec1, spec2)
-        @test ustrip.(Spectra.wave(spec1)) ≈ ustrip.(unit(eltype(Spectra.wave(spec1))), Spectra.wave(spec2))
+        #spec1 = mock_spectrum(Integer(1e4), use_units=true)
+        #spec2 = mock_spectrum(Integer(1e3), use_units=true)
+        #spec1.wave = uconvert.(u"cm", spec1.wave)
+        #resample!(spec1, spec2)
+        #@test ustrip.(Spectra.wave(spec1)) ≈ ustrip.(unit(eltype(Spectra.wave(spec1))), Spectra.wave(spec2))
 
         # address bug when doing the same thing but affecting spec2
-        spec1 = mock_spectrum(Integer(1e4), use_units=true)
-        spec2 = mock_spectrum(Integer(1e3), use_units=true)
-        spec2.wave = uconvert.(u"cm", spec2.wave)
-        resample!(spec1, spec2)
-        @test ustrip.(Spectra.wave(spec1)) ≈ ustrip.(unit(eltype(Spectra.wave(spec1))), Spectra.wave(spec2))
+        #spec1 = mock_spectrum(Integer(1e4), use_units=true)
+        #spec2 = mock_spectrum(Integer(1e3), use_units=true)
+        #spec2.wave = uconvert.(u"cm", spec2.wave)
+        #resample!(spec1, spec2)
+        #@test ustrip.(Spectra.wave(spec1)) ≈ ustrip.(unit(eltype(Spectra.wave(spec1))), Spectra.wave(spec2))
     end
 end
